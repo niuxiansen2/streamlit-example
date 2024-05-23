7b18acecb19cb3a3a0cb28de655643a41b535f3a
nwj <1225708606@qq.com>
2024-05-23 16:06:32 +0800

    new

--- HomePage.py
+++ HomePage.py
@@ -11,18 +11,16 @@ with st.sidebar:
         sac.MenuItem('主页', icon='house-fill', ),
         # sac.MenuItem('业务运营智能审批要件', icon='box-fill'),
         sac.MenuItem("多模态文件信息比对测试", icon='apple', children=[
-            sac.MenuItem('合同数据',children=
-                        [sac.MenuItem('贷款业务')]
-                         ),
-            sac.MenuItem('调查报告'),
-            sac.MenuItem('授信批复'),
-            sac.MenuItem('申请表'),
+            sac.MenuItem('贷款业务'),
+            # sac.MenuItem('调查报告'),
+            # sac.MenuItem('授信批复'),
+            # sac.MenuItem('申请表'),
             sac.MenuItem('更多类型文件'),
         ]),
         sac.MenuItem('合同版本智能比对', icon='git', description='other items'),
         sac.MenuItem("测试报告智能检核", icon='google', description='item description'),
         sac.MenuItem('UI智能核验', icon='gitlab'),
-        sac.MenuItem('wechat', icon='wechat')]
+        sac.MenuItem('更多功能', icon='wechat')]
         , key="menu", height=500, size=18, open_index=[1])
     for _ in range(20):
         st.write(" ")
